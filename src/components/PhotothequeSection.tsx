6bdc1538e2ff19860147bb70f4ab07152db96dcf
Alok <engineeralok07@gmail.com>
2025-11-25 21:04:16 +0530

    photo

--- src/components/PhotothequeSection.tsx
+++ src/components/PhotothequeSection.tsx
@@ -5,8 +5,19 @@ import Image from "next/image";
 export default function PhotothequeSection() {
   return (
     <section className="phototheque-section fr-mt-6w">
-      <div className="fr-grid-row fr-grid-row--gutters items-center">
-        <div className="fr-col-12 phototheque-content-col order-2 md:order-1">
+      <div className="fr-grid-row fr-grid-row--gutters phototheque-container">
+        <div className="fr-col-12 phototheque-image-col order-2 md:order-1">
+          <div className="phototheque-image-placeholder">
+            <Image
+              src="/images/photo.jpeg"
+              alt="Les images des actions du ministère enregistrées"
+              width={1200}
+              height={750}
+              className="phototheque-image"
+            />
+          </div>
+        </div>
+        <div className="fr-col-12 phototheque-content-col order-1 md:order-2">
           <div className="phototheque-content">
             <h2 className="phototheque-title">
               <span className="underline">Photothèque</span>
@@ -19,17 +30,6 @@ export default function PhotothequeSection() {
                 VOIR PLUS/
               </a>
             </div>
-          </div>
-        </div>
-        <div className="fr-col-12 phototheque-image-col order-1 md:order-2">
-          <div className="phototheque-image-placeholder">
-            <Image
-              src="/images/photo.jpeg"
-              alt="Les images des actions du ministère enregistrées"
-              width={1200}
-              height={750}
-              className="phototheque-image"
-            />
           </div>
         </div>
       </div>
